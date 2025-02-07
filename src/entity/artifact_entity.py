b219cf7fa8f8b9e9fc4da9e8196dc1aa21013abd
Harsh Kumar <harshks2320@gmail.com>
2025-02-07 21:51:44 +0530

    artifacts_entity: Model trainer artifact added

--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -26,17 +26,5 @@ class ClassificationMetricArtifact:
 
 @dataclass
 class ModelTrainerArtifact:
-    trained_model_file_path:str 
-    metric_artifact:ClassificationMetricArtifact
-
-@dataclass
-class ModelEvaluationArtifact:
-    is_model_accepted:bool
-    changed_accuracy:float
-    s3_model_path:str 
-    trained_model_path:str
-
-@dataclass
-class ModelPusherArtifact:
-    bucket_name:str
-    s3_model_path:str
+    trained_model_artifact: str
+    metric_artifact: ClassificationMetricArtifact
