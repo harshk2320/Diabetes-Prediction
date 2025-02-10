21e2158c36d278af3f46f6541af68d3a6862f05e
Harsh Kumar <harshks2320@gmail.com>
2025-02-10 14:19:29 +0530

    prediction_pipeline.py: added libraries

---
+++ src/pipline/prediction_pipeline.py
@@ -0,0 +1,7 @@
+import sys
+import pandas as pd
+from src.logger import logging
+from src.exception import MyException
+from entity.s3_estimator import Proj1Estimator
+from src.entity.config_entity import DiabetesPredictionConfig
+
